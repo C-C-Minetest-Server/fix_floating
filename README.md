875aa223c6be9e634ddac7d3d3bad091f9494b2a
1F616EMO <root@1f616emo.xyz>
2024-01-30 19:26:03 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# fix_floating
+# Fixing floating sands
+
+This mod enhances map generation; it replaces any falling blocks above air into their solid form. Currently, Minetest Game is supported. Other mods could register their list of falling nodes by adding entries into the `fix_floating.list_nodes` table.
+
+Now, no sand nor gravel would fall upon contact; but this raises another question; what keeps the sandstones in place? The 9.81 meters per second gravity due to Earth is being ignored...
+
+## Known Bugs
+
+* If a WorldEdit operation places supported falling blocks out of the currently emerged areas without support, they would be transformed into solid blocks. This is due to those modifications being done pre-emerge; the phenomenon of such blocks being affected by map generation (e.g. ore generation) is a known upstream bug.
---
+++ init.lua
@@ -0,0 +1,67 @@
+-- fix_floating/init.lua
+-- Avoid hanging sands and gravels
+--[[
+    Copyright (C) 2023  1F616EMO
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+    USA
+]]
+
+fix_floating = {
+    list_nodes = {}
+}
+
+local ID = minetest.get_content_id
+
+if minetest.get_modpath("default") then
+    fix_floating.list_nodes[ID("default:sand")] = ID("default:sandstone")
+    fix_floating.list_nodes[ID("default:silver_sand")] = ID("default:silver_sandstone")
+    fix_floating.list_nodes[ID("default:desert_sand")] = ID("default:desert_sandstone")
+    fix_floating.list_nodes[ID("default:gravel")] = ID("default:stone")
+end
+
+minetest.register_on_generated(function(minp, maxp, blockseed)
+    local vm, vminp, vmaxp = minetest.get_mapgen_object("voxelmanip")
+    local va = VoxelArea(vminp, vmaxp)
+
+    -- Load at least one block below
+    do
+        local nminp = vector.new(minp.x, minp.y - 1, minp.z)
+        local nmaxp = vector.new(maxp.x, minp.y - 1, maxp.z)
+
+        vminp, vmaxp = vm:read_from_map(nminp, nmaxp)
+    end
+
+    do
+        local data = vm:get_data()
+
+        -- Iterate through generated blocks
+        for i in va:iterp(minp, maxp) do
+            local below = va:indexp(vector.subtract(va:position(i), {x=0, y=1, z=0}))
+
+            if data[below] == minetest.CONTENT_AIR then
+                local targ = fix_floating.list_nodes[data[i]]
+                if targ then
+                    data[i] = targ
+                end
+            end
+        end
+
+        vm:set_data(data)
+    end
+
+    vm:write_to_map(true)
+end)
+
